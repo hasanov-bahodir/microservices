08ee14e99cbc59b40ed5a36b23395a902d194d14
Bahodir Hasanov <bahodir.hasanov@paybek.io>
2022-09-27 17:21:42 +0500

    every thing is test and learned as much as i can

--- customer/src/main/java/com/paybek/CustomerApplication.java
+++ customer/src/main/java/com/paybek/CustomerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(
         scanBasePackages = {
+                "com.paybek.customer",
                 "com.paybek.amqp"
         }
 )
--- notification/src/main/java/com/paybek/NotificationApplication.java
+++ notification/src/main/java/com/paybek/NotificationApplication.java
@@ -19,20 +19,20 @@ public class NotificationApplication {
         SpringApplication.run(NotificationApplication.class, args);
     }
 
-//    @Bean
-//    CommandLineRunner commandLineRunner(
-//            RabbitMQMessageProducer producer,
-//            NotificationConfig notificationConfig
-//    ) {
-//        return args -> {
-//            producer.publish(
-//                    new Person("Ali", 26),
-//                    notificationConfig.getInternalExchange(),
-//                    notificationConfig.getInternalNotificationRoutingKey()
-//            );
-//        };
-//    }
-//
-//    record Person(String name, int age) {
-//    }
+    @Bean
+    CommandLineRunner commandLineRunner(
+            RabbitMQMessageProducer producer,
+            NotificationConfig notificationConfig
+    ) {
+        return args -> {
+            producer.publish(
+                    new Person("Ali", 26),
+                    notificationConfig.getInternalExchange(),
+                    notificationConfig.getInternalNotificationRoutingKey()
+            );
+        };
+    }
+
+    record Person(String name, int age) {
+    }
 }
